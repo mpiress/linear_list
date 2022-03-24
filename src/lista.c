d0d9603964875e424f347f8e65567b30c73cd978
mpiress <michel.silva@gmail.com>
2022-03-24 16:14:04 -0300

    update code

---
+++ src/lista.c
@@ -0,0 +1,52 @@
+#include "lista.h"
+
+void Swap(Item *a, Item *b){
+	Item aux;
+	aux = *a;
+	*a  = *b;
+	*b  = aux;
+}
+
+void FLVazia(Lista *l){
+	l->first = 0;
+	l->last  = 0;
+}
+
+void LInsert(Lista *l, Item d){
+	if (l->last >= MAXTAM){
+		printf("LISTA CHEIA!\n");
+	}else{
+		l->vet[l->last] = d;
+		l->last ++;
+	}
+}
+
+void LRemove(Lista *l, Item d){
+	bool ok = false;
+
+	if(l->first == l->last)
+		printf("LISTA VAZIA!\n");
+	else{
+		for(int i=l->first; i<l->last; i++)
+			if(l->vet[i].val == d.val){
+				Swap(&l->vet[i], &l->vet[i+1]);
+				ok = true;	
+			}
+	
+		if(ok)
+			l->last --;
+	}
+}
+
+
+void LImprime(Lista *l){
+	for(int i=l->first; i<l->last; i++)
+		printf("%d\t", l->vet[i].val);
+	printf("\n");
+}
+
+
+
+
+
+
---
+++ src/lista.h
@@ -0,0 +1,27 @@
+#ifndef LISTA_H 
+#define LISTA_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define MAXTAM 10
+
+typedef struct Item{
+	int val;
+}Item;
+
+typedef struct Lista{
+	Item vet[MAXTAM];
+	int first;
+	int last;
+}Lista;
+
+
+void FLVazia(Lista *l);
+void LInsert(Lista *l, Item d);
+void LRemove(Lista *l, Item d);
+void Swap(Item *a, Item *b);
+void LImprime(Lista *l);
+
+#endif
